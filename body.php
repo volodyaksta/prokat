c8037da6fdbec31ee74609223563e4055595be86
volodyaksta <vladimirfriday13@gmail.com>
2021-05-18 14:09:53 +0300

    1

---
+++ body.php
@@ -0,0 +1,17 @@
+﻿<div class="page_header">
+О компании
+</div>
+<h1 style="text-align:center"><span style="font-family:Comic Sans MS,cursive">Компания «‎Сами катайтесь»‎ предоставляет на выбор большое количество автомобилей</span></h1>
+
+<hr />
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Мы сдаем в аренду только качественные автомобили&nbsp;</div>
+
+<h2 style="font-style:italic"><img class="left" src="https://media-cdn.tripadvisor.com/media/photo-s/10/af/e6/b8/caption.jpg" /></h2>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Самая маленькая цена среди компаний ориентированных на данную аудиторию</div>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">Наш квалифицированный персонал ответит вам на все вопросы</div>
+
+<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">
+<p>Все наши клиенты положительно отзываются о нас!</p>
+</div>
---
+++ lib/db_connect.php
@@ -0,0 +1,5 @@
+<?
+$link = mysqli_connect('localhost', 'root', '', 'prokat') or dir("Connection error!");
+session_start();
+$site_name = "Прокат автомобилей «Сами катайтесь»‎‎";
+?>
